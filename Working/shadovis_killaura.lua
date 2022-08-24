a1cdef35afd25f9e921e4b87c100ddcd557665a4
Dory <78720364+dooory@users.noreply.github.com>
2022-08-24 17:49:30 +1200

    Update shadovis_killaura.lua

--- Working/shadovis_killaura.lua
+++ Working/shadovis_killaura.lua
@@ -1,24 +1,20 @@
 --// game link: https://www.roblox.com/games/9585537847/SHADOVIS-RPG
 
 getgenv().MaxRange = 25
-getgenv().MaxThreads = 9e9
-getgenv().CurrentThreadCount = 0
 
 local Run = game:GetService("RunService")
 local plr = game.Players.LocalPlayer
 
-local WeaponData = require(game:GetService("ReplicatedStorage").WeaponData)
-
 local WeaponName
 
 for i,v in next, plr.Character.Equipment:GetChildren() do
-    if WeaponData[v.Name] then
+    if v:FindFirstChild("CharacterWeld") and v.CharacterWeld:FindFirstChild("SwordGrip") then
         WeaponName = v.Name
     end
 end
 
 plr.Character.Equipment.ChildAdded:Connect(function(v)
-    if WeaponData[v.Name] then
+    if v:FindFirstChild("CharacterWeld") and v.CharacterWeld:FindFirstChild("SwordGrip") then
         WeaponName = v.Name
     end
 end)
@@ -32,7 +28,7 @@ Run.Heartbeat:Connect(function()
     
     for i,v in next, workspace.NPCs:GetChildren() do
         local dist = (plr.Character.PrimaryPart.Position - v.PrimaryPart.Position).Magnitude
-        
+
         if dist < MaxRange then
             DamageMob(v)
         end
